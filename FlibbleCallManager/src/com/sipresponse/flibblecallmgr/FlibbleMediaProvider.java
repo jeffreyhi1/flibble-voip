2fd9f37924c1d0f2f20fc3d3fa3d7bdc2f3316b1
reliagility <reliagility@4c746b2d-a82a-0410-9326-4b6ed91c80fc>
2007-03-02 03:15:38 +0000

    Copyright notice, javadoc comments.

--- FlibbleCallManager/src/com/sipresponse/flibblecallmgr/FlibbleMediaProvider.java
+++
@@ -1,6 +0,0 @@
-package com.sipresponse.flibblecallmgr;
-
-public class FlibbleMediaProvider
-{
-
-}
--- FlibbleCallManager/src/com/sipresponse/flibblecallmgr/FlibbleUiProvider.java
+++ FlibbleCallManager/src/com/sipresponse/flibblecallmgr/FlibbleUiProvider.java
@@ -1,5 +1,22 @@
+/*
+ * @(#) FlibbleUIProvider
+ *
+ * Copyright (c) 2007 Michael D. Cohen.  All rights reserved.
+ * 
+ * @author mike@sipresponse.com
+ */
 package com.sipresponse.flibblecallmgr;
 
+/**
+ * Interface which defines a UserAgent UI entity. 
+ * An object implementing this interface can be supplied to
+ * the CallManager's initalize function.
+ * If the UserAgent is headless (provides no UI),
+ * the CallManager can be initialized without a
+ * FlibbleUIProvider.
+ * 
+ * @author Mike Cohen
+ */
 public interface FlibbleUiProvider
 {
 
