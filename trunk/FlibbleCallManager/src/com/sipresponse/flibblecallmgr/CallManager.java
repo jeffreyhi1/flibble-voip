85270225b858c9c9b3afca3115e62a111472831f
reliagility <reliagility@4c746b2d-a82a-0410-9326-4b6ed91c80fc>
2007-03-02 03:15:38 +0000

    Copyright notice, javadoc comments.

--- trunk/FlibbleCallManager/src/com/sipresponse/flibblecallmgr/CallManager.java
+++ trunk/FlibbleCallManager/src/com/sipresponse/flibblecallmgr/CallManager.java
@@ -1,10 +1,32 @@
+/*
+ * @(#) CallManager
+ *
+ * Copyright (c) 2007 Michael D. Cohen.  All rights reserved.
+ * 
+ * @author mike@sipresponse.com
+ */
 package com.sipresponse.flibblecallmgr;
 
+import com.sipresponse.flibblecallmgr.media.FlibbleMediaProvider;
+
+/**
+ * Singleton object is central to flibble-voip.
+ * Allows for call control and media control.
+ * Provides a simple to use 
+ * 
+ * @author Mike Cohen
+ *
+ */
 public class CallManager
 {
     private FlibbleMediaProvider mediaProvider;
     private FlibbleUiProvider uiProvider;
     private static CallManager instance;
+    
+    /**
+     * Obtains the one and only instance of the CallManager.
+     * @return The CallManager instance
+     */
     public static synchronized CallManager getInstance()
     {
         if (null == instance)
@@ -16,10 +38,18 @@ public class CallManager
     private CallManager()
     {
     }
+    /**
+     * 
+     * @param uiProvider The UI implementation.  Supply a null for an application 
+     * with no user interface.
+     * @param mediaProvider The media implementation.  Supply a null
+     * for an application with no media control.
+     */
     public void initialize(FlibbleUiProvider uiProvider,
                            FlibbleMediaProvider mediaProvider)
     {
-        
+        this.uiProvider = uiProvider;
+        this.mediaProvider = mediaProvider;
     }
                            
 }
---
+++ trunk/FlibbleCallManager/src/com/sipresponse/flibblecallmgr/media/FlibbleMediaProvider.java
@@ -0,0 +1,17 @@
+/*
+ * @(#) FlibbleMediaProvider
+ *
+ * Copyright (c) 2007 Michael D. Cohen.  All rights reserved.
+ * 
+ * @author mike@sipresponse.com
+ */
+package com.sipresponse.flibblecallmgr.media;
+
+/**
+ * Abstract class defining the media control interface. 
+ * @author Mike Cohen
+ */
+public class FlibbleMediaProvider
+{
+
+}
