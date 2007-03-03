8f9f769572038b2257e1a4dd4a3c895db98f34aa
reliagility <reliagility@4c746b2d-a82a-0410-9326-4b6ed91c80fc>
2007-03-03 02:51:12 +0000

    Dependencies.

--- FlibbleCallManager/src/com/sipresponse/flibblecallmgr/CallManager.java
+++ FlibbleCallManager/src/com/sipresponse/flibblecallmgr/CallManager.java
@@ -1,55 +1,76 @@
-/*
- * @(#) CallManager
+/*******************************************************************************
+ *   Copyright 2007 SIP Response
+ *   Copyright 2007 Michael D. Cohen
  *
- * Copyright (c) 2007 Michael D. Cohen.  All rights reserved.
- * 
- * @author mike@sipresponse.com
- */
+ *      mike _AT_ sipresponse.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
 package com.sipresponse.flibblecallmgr;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.sipresponse.flibblecallmgr.media.FlibbleMediaProvider;
 
 /**
- * Singleton object is central to flibble-voip.
+ * Object is central to flibble-voip.
  * Allows for call control and media control.
- * Provides a simple to use 
+ * Provides a simple to use interface for controlling voip calls. 
  * 
  * @author Mike Cohen
  *
  */
 public class CallManager
 {
+    private boolean useSoundCard;
     private FlibbleMediaProvider mediaProvider;
     private FlibbleUiProvider uiProvider;
-    private static CallManager instance;
-    
+    private ConcurrentHashMap callMap = new ConcurrentHashMap(); 
+
     /**
-     * Obtains the one and only instance of the CallManager.
-     * @return The CallManager instance
+     * Constructor.
+     *
      */
-    public static synchronized CallManager getInstance()
-    {
-        if (null == instance)
-        {
-            instance = new CallManager();
-        }
-        return instance;
-    }
-    private CallManager()
+    public CallManager()
     {
     }
+
     /**
-     * 
+     * Initializes the CallManager.  The object must not be used
+     * before initialization.
+     *
+     * @param localIp
+     * @param udpSipPort
+     * @param mediaPortStart
+     * @param mediaPortEnd
+     * @param enableStun
      * @param uiProvider The UI implementation.  Supply a null for an application 
-     * with no user interface.
-     * @param mediaProvider The media implementation.  Supply a null
-     * for an application with no media control.
+     *          with no user interface.
+     *          for an application with no media control.
+     * @param useSoundCard True if the application wishes to utilize audio hardware.
+     *          Otherwise, false.
+     * 
      */
-    public void initialize(FlibbleUiProvider uiProvider,
-                           FlibbleMediaProvider mediaProvider)
+    public void initialize(String localIp,
+                           int udpSipPort,
+                           int mediaPortStart,
+                           int mediaPortEnd,
+                           boolean enableStun,
+                           FlibbleUiProvider uiProvider,
+                           boolean useSoundCard)
     {
         this.uiProvider = uiProvider;
-        this.mediaProvider = mediaProvider;
+        this.useSoundCard = useSoundCard;
     }
                            
 }
