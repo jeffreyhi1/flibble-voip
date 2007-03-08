7e9b618c3629e49588b53bdde74e13e07d9a6f8b
reliagility <reliagility@4c746b2d-a82a-0410-9326-4b6ed91c80fc>
2007-03-08 02:48:26 +0000

    exiting on a call failure

--- trunk/PlaceCall/src/com/sipresponse/placecall/PlaceCall.java
+++ trunk/PlaceCall/src/com/sipresponse/placecall/PlaceCall.java
@@ -1,23 +1,44 @@
 package com.sipresponse.placecall;
 
 import com.sipresponse.flibblecallmgr.CallManager;
+import com.sipresponse.flibblecallmgr.Event;
+import com.sipresponse.flibblecallmgr.EventCode;
+import com.sipresponse.flibblecallmgr.EventType;
+import com.sipresponse.flibblecallmgr.FlibbleListener;
 
-public class PlaceCall
+public class PlaceCall implements FlibbleListener
 {
+    private boolean shouldExit = false;
 
     public void go()
     {
         CallManager callMgr = new CallManager();
-        callMgr.initialize("192.168.0.105",
+        callMgr.initialize("192.168.0.101",
                 5060,
                 9300,
                 9400,
                 null,
                 5060,
                 false, true);
-        String lineHandle = callMgr.addLine("sip:foo@192.168.0.105", "Foo Bar", false);
+        callMgr.addListener(this);
+        String lineHandle = callMgr.addLine("sip:foo@192.168.0.101", "Foo Bar", false);
         String callHandle = callMgr.createCall(lineHandle, "sip:reliagility@192.168.0.103");
         callMgr.placeCall(callHandle);
+        
+        while (shouldExit == false)
+        {
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        callMgr.destroy();
+        callMgr = null;
 
     }
     /**
@@ -28,6 +49,18 @@ public class PlaceCall
         PlaceCall placeCall = new PlaceCall();
         placeCall.go();
 
+    }
+    public boolean onEvent(Event event)
+    {
+        
+        if (event.getEventType() == EventType.CALL)
+        {
+            if (event.getEventCode() == EventCode.CALL_FAILED)
+            {
+                shouldExit = true;
+            }
+        }
+        return false;
     }
 
 }
