fd6d8e6e25e69d3053363356509d65fd026a89b6
reliagility <reliagility@4c746b2d-a82a-0410-9326-4b6ed91c80fc>
2007-03-31 23:08:52 +0000

    copyright notice

--- trunk/PlaceCall/src/com/sipresponse/placecall/PlaceCall.java
+++ trunk/PlaceCall/src/com/sipresponse/placecall/PlaceCall.java
@@ -1,3 +1,21 @@
+/*******************************************************************************
+ *   Copyright 2007 SIP Response
+ *   Copyright 2007 Michael D. Cohen
+ *
+ *      mike _AT_ sipresponse.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
 package com.sipresponse.placecall;
 
 import com.sipresponse.flibblecallmgr.CallManager;
@@ -16,13 +34,13 @@ public class PlaceCall implements FlibbleListener
 
     private void go()
     {
-        callMgr.initialize("192.168.0.103",
+        callMgr.initialize("192.168.0.105",
                 5060,
                 9300,
                 9400,
                 null,
                 5060,
-                false, null, true);
+                false, null, true, null);
         callMgr.addListener(this);
         
         // create a registered line
