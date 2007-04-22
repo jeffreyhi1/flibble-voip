dd666eeeb3a812783d4e789c0890cd0c1e099804
reliagility <reliagility@4c746b2d-a82a-0410-9326-4b6ed91c80fc>
2007-04-22 19:24:48 +0000

    New invite handler

--- FlibbleCallManager/src/com/sipresponse/flibblecallmgr/internal/handlers/Handler.java
+++ FlibbleCallManager/src/com/sipresponse/flibblecallmgr/internal/handlers/Handler.java
@@ -1,9 +1,30 @@
+/*******************************************************************************
+ *   Copyright 2007 SIP Response
+ *   Copyright 2007 Michael D. Cohen
+ *
+ *      mike _AT_ sipresponse.com
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ ******************************************************************************/
 package com.sipresponse.flibblecallmgr.internal.handlers;
 
 import javax.sip.RequestEvent;
+import javax.sip.ServerTransaction;
+import javax.sip.message.Response;
 
 import com.sipresponse.flibblecallmgr.CallManager;
 import com.sipresponse.flibblecallmgr.internal.Call;
+import com.sipresponse.flibblecallmgr.internal.FlibbleSipProvider;
 import com.sipresponse.flibblecallmgr.internal.InternalCallManager;
 import com.sipresponse.flibblecallmgr.internal.Line;
 
@@ -40,4 +61,31 @@ public abstract class Handler
     }
     
     public abstract void execute();
+    
+    public void sendResponse(int statusCode)
+    {
+        FlibbleSipProvider flibbleProvider = InternalCallManager.getInstance()
+            .getProvider(callMgr);
+        Response response = null;
+        try
+        {
+            response = flibbleProvider.messageFactory.createResponse(100, requestEvent.getRequest());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        if (null != response)
+        {
+            ServerTransaction st = requestEvent.getServerTransaction();
+            try
+            {
+                st.sendResponse(response);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
 }
