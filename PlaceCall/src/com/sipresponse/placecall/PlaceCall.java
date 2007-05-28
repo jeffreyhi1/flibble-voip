af4ca23c1e27957e60063264fe1791bc219b4831
reliagility <reliagility@4c746b2d-a82a-0410-9326-4b6ed91c80fc>
2007-05-28 03:32:24 +0000

    more comments

--- PlaceCall/src/com/sipresponse/placecall/PlaceCall.java
+++ PlaceCall/src/com/sipresponse/placecall/PlaceCall.java
@@ -40,25 +40,21 @@ public class PlaceCall implements FlibbleListener
     {
         try
         {
-            callMgr.initialize(InetAddress.getLocalHost().getHostAddress(),
-                    5060,
-                    9300,
-                    9400,
-                    "sphone.vopr.vonage.net",
-                    5060,
-                    null,
-                    true,
-                    null);
+            callMgr.initialize(InetAddress.getLocalHost().getHostAddress(),  // address to bind to
+                    5060, // port to bind to 
+                    9300, // start media port range
+                    9400, // end media port range
+                    "sphone.vopr.vonage.net", // proxy address
+                    5060, // proxy port
+                    null, // stun server
+                    true, // use sound card
+                    null); // media filename
+            callMgr.addListener(this);  // this class implements the FlibbleListener interface
         }
-        catch (IllegalArgumentException e1)
+        catch (Exception e1)
         {
             e1.printStackTrace();
         }
-        catch (UnknownHostException e1)
-        {
-            e1.printStackTrace();
-        }
-        callMgr.addListener(this);
         
         // create a registered line
         lineHandle = callMgr.addLine("sip:17815552814@sphone.vopr.vonage.net", "Foo Bar", true, 20, "password");        
